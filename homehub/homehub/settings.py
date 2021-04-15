2c582c56784ccbaf2635f8c82f5222aa90a6d42f
Jon <stricoff92@gmail.com>
2021-04-14 22:31:30 -0400

    wip

--- homehub/homehub/settings.py
+++ homehub/homehub/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CHROME_DRIVER_PATH = applocals.CHROME_DRIVER_PATH
+WEATHER_API_KEY = applocals.WEATHER_API_KEY
+
