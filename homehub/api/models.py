55096e98701a13b8f6d17400bb5d3d388724a69b
Jon <stricoff92@gmail.com>
2021-04-15 18:44:25 -0400

    wip

--- homehub/api/models.py
+++ homehub/api/models.py
@@ -11,3 +11,8 @@ class WeatherSource(models.Model):
 
     longitude = models.CharField(max_length=20)
     latitude = models.CharField(max_length=20)
+
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return f"<WS{self.pk} {self.name} {self.latitude} {self.longitude}>"
