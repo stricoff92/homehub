6d6b23af202a95133e44d64b8b67c4690774d141
Jon <stricoff92@gmail.com>
2021-04-15 19:00:28 -0400

    wip

--- homehub/homehub/settings.py
+++ homehub/homehub/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "website/templates/angular_static"),
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- homehub/website/urls.py
+++ homehub/website/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import url, include
 from django.urls import path
 
+from website import views
 
 urlpatterns = [
+    path("", views.landing, name="app-landing")
 ]
