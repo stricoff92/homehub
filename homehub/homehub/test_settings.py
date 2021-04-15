c67fe2ac075300acddf0fa7460625a6473050bc0
Jon <stricoff92@gmail.com>
2021-04-14 20:47:48 -0400

    first commit

---
+++ homehub/homehub/test_settings.py
@@ -0,0 +1,33 @@
+
+import os.path
+
+from .settings import *
+
+ENV = "TESTING"
+DEBUG = True
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+AUTH_PASSWORD_VALIDATORS = []
+
+
+# Faster insecure hashing for testing only
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# File Based DB for Faster setup and teardown
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'testdb.sqlite3'),
+    }
+}
+
+# Disable cache based rate throttling
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
