516398ec3728863c599393f79063371651561689
Jon <stricoff92@gmail.com>
2021-04-19 22:39:34 -0400

    wi[

--- homehub/api/management/commands/update_bike_cache.py
+++ homehub/api/management/commands/update_bike_cache.py
@@ -3,11 +3,17 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from api.lib import bikes
+from api.lib import bikes, hubstate, script_logger
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        logger = script_logger.get_hub_logger()
+        hs = hubstate.HubState()
+        if not hs.getkey(hs.STATE_KEY_IS_ONLINE):
+            logger.debug("skipping weather update")
+            return
+
         bikes.update_cache()
 
